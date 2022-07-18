f8b599c70e205e82561d9f6290fac04a80ab3dfc
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-07-18 17:31:33 -0500

    pipes | Terminando seccion de Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitán América';
+
+  arreglo= [1,2,3,4,5,6,7,8,9,10];
+
+  PI: number = Math.PI;
+  porcentaje: number = 0.235;
+  salario: number = 1234.5
+
+  heroe = {
+    nombre:'Logan',
+    clave:'Wolverine',
+    edad:500,
+    direccion:{
+      calle:'Primera',
+      casa:20
+    }
+  }
+
+  valorPromesa = new Promise<string>( (resolve) =>{
+    setTimeout(() => {
+      resolve('llego la data')
+    }, 4500);
+  })
+
+  fecha:Date = new Date();
+
+  idioma = 'es';
+
+  nombre2: string = 'RicArdO OmaR LuGo VaRgaS';
+
+  videoUrl: string = "https://www.youtube.com/embed/x90oqr7k9g0";
+
+  activar : boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+import localeES from '@angular/common/locales/es'
+import localeFR from '@angular/common/locales/fr'
+
+registerLocaleData(localeES)
+registerLocaleData(localeFR)
+
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:LOCALE_ID,
+      useValue:'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
